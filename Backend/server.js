024c0f03763feee31a14e5faf2b5b26e2c832dab
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-23 17:39:27 +0545

    added requried folders

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
