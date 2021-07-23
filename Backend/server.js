f1e0c3ba285eead21596aabeff5d1c2b4661cef8
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-07-23 17:59:03 +0545

    Database connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,40 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+// Catching uncaught exceptions
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED EXCEPTION 💥 Shutting down...");
+  process.exit(1);
+});
+
+//
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const port = process.env.PORT || 8000;
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection established 😎"));
 
-app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+// START SERVER
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port} 🔥`)
+);
+
+//Crash Server
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
